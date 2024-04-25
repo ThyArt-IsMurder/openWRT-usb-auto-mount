bdf82c3932eacabb51e02271909c4c46ec32ab5c
Afshin <37227277+ThyArt-IsMurder@users.noreply.github.com>
2024-04-25 14:35:19 -0700

    Update passthewall.sh

--- passthewall.sh
+++ passthewall.sh
@@ -9,11 +9,10 @@ GRAY='\033[0;37m'
 NC='\033[0m' # No Color
 
 echo -e "${YELLOW} 
- ____   _    ____ ____ _____ _   _ _______        ___    _     _     
-|  _ \ / \  / ___/ ___|_   _| | | | ____\ \      / / \  | |   | |    
-| |_) / _ \ \___ \___ \ | | | |_| |  _|  \ \ /\ / / _ \ | |   | |    
-|  __/ ___ \ ___) |__) || | |  _  | |___  \ V  V / ___ \| |___| |___ 
-|_| /_/   \_\____/____/ |_| |_| |_|_____|  \_/\_/_/   \_\_____|_____|
+ _____ _____ _____ _____ _____ _____ _____ _ _ _ _____ __    __    
+|  _  |  _  |   __|   __|_   _|  |  |   __| | | |  _  |  |  |  |   
+|   __|     |__   |__   | | | |     |   __| | | |     |  |__|  |__ 
+|__|  |__|__|_____|_____| |_| |__|__|_____|_____|__|__|_____|_____|
 
 echo "Running as root..."
 sleep 2
@@ -25,3 +24,48 @@ DISK="/dev/sda"
 parted -s ${DISK} -- mklabel gpt mkpart extroot 2048s -2048s
 DEVICE="${DISK}1"
 mkfs.ext4 -L extroot ${DEVICE}
+
+sleep 2
+
+### Configure the extroot mount entry. ###
+
+eval $(block info ${DEVICE} | grep -o -e 'UUID="\S*"')
+eval $(block info | grep -o -e 'MOUNT="\S*/overlay"')
+uci -q delete fstab.extroot
+uci set fstab.extroot="mount"
+uci set fstab.extroot.uuid="${UUID}"
+uci set fstab.extroot.target="${MOUNT}"
+uci commit fstab
+
+sleep 2
+
+### Transfer the content of the current overlay to the external drive. ###
+
+mount ${DEVICE} /mnt
+tar -C ${MOUNT} -cvf - . | tar -C /mnt -xf -
+
+sleep 5
+
+### Configure a mount entry for the the original overlay. ###
+
+DEVICE="$(block info | sed -n -e '/MOUNT="\S*\/overlay"/s/:\s.*$//p')"
+uci -q delete fstab.rwm
+uci set fstab.rwm="mount"
+uci set fstab.rwm.device="${ORIG}"
+uci set fstab.rwm.target="/rwm"
+uci commit fstab
+
+echo -e "${GREEN}Done ! Your Router Will Be reboot After 5 Seconds ... ${NC}"
+
+echo -e "${MAGENTA} 
+ █████╗ ███████╗███████╗██╗  ██╗██╗███╗   ██╗
+██╔══██╗██╔════╝██╔════╝██║  ██║██║████╗  ██║
+███████║█████╗  ███████╗███████║██║██╔██╗ ██║
+██╔══██║██╔══╝  ╚════██║██╔══██║██║██║╚██╗██║
+██║  ██║██║     ███████║██║  ██║██║██║ ╚████║
+╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
+
+
+sleep 5
+
+reboot
